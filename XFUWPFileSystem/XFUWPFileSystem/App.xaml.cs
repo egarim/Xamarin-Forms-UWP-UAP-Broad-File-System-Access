2f16472ef319e865383d33ecc1d4033e2ed399fb
Joche <Joche@DESKTOP-O5CUHO6>
2019-01-26 10:58:10 +0300

    CompleteDemo

--- XFUWPFileSystem/XFUWPFileSystem/App.xaml.cs
+++ XFUWPFileSystem/XFUWPFileSystem/App.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Windows.Storage;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,10 +13,27 @@ namespace XFUWPFileSystem
         public App()
         {
             InitializeComponent();
-
+           
             MainPage = new MainPage();
         }
-
+        public async void DirSearch(string sDir)
+        {
+            var ext = new List<string> { ".txt", ".zip" };
+            var folder = await StorageFolder.GetFolderFromPathAsync(sDir);
+            //var ExactLocalFolder = await ApplicationData.Current.LocalFolder.CreateFolderAsync("ExactLocalFolder");
+            foreach (var file in await folder.GetFilesAsync())
+            {
+                Debug.WriteLine(file.Name);
+                
+            }
+            //copy the files to the folder that you want to save the extract file into
+            //foreach (var file in await ExactLocalFolder.GetFilesAsync())
+            //{
+            //    await file.CopyAsync(folder);
+            //}
+            ////delete the folder in the local folder
+            //await ExactLocalFolder.DeleteAsync();
+        }
         protected override void OnStart()
         {
             // Handle when your app starts
--- XFUWPFileSystem/XFUWPFileSystem/MainPage.xaml.cs
+++ XFUWPFileSystem/XFUWPFileSystem/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 using Xamarin.Forms;
 
 namespace XFUWPFileSystem
@@ -12,6 +14,21 @@ namespace XFUWPFileSystem
         public MainPage()
         {
             InitializeComponent();
+            DirSearch(@"C:\");
+        }
+        public async void DirSearch(string sDir)
+        {
+            List<string> Directories = new List<string>();
+            var folder = await StorageFolder.GetFolderFromPathAsync(sDir);
+      
+            foreach (var Folder in await folder.GetFoldersAsync())
+            {
+                Debug.WriteLine(Folder.Name);
+                Directories.Add(Folder.Name);
+
+            }
+            this.Fs.ItemsSource = Directories;
+           
         }
     }
 }
